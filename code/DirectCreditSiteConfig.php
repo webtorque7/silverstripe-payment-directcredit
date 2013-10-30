3b094f997087bed9a980d61f40f5f9a14353f80d
Conrad <conrad@webtorque.co.nz>
2013-10-31 12:17:51 +1300

    Message for direct credit payments

---
+++ code/DirectCreditSiteConfig.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: User
+ * Date: 31/10/13
+ * Time: 9:29 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class DirectCreditSiteConfig extends DataExtension
+{
+        private static $db = array(
+                'DirectCreditText' => 'HTMLText'
+        );
+
+        public function getCMSFields(FieldList $fields) {
+                $fields->addFieldToTab('Root.Payments.DirectCredit', HtmlEditorField::create('DirectCreditText', 'Direct Credit Text')
+                        ->setDescription('Instructions for customer on how to pay (bank account etc)')
+                );
+        }
+}
